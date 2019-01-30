4a4ef073e1b467132a03354309a96b6319941c21
Jor_Poon <jormpoon@gmail.com>
2019-01-30 17:33:50 -0500

    article.js followed instructions to add article class

--- Article/Article.js
+++ Article/Article.js
@@ -10,11 +10,17 @@ class Article {
     this.expand.innerHTML = "Expand";
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+
+    this.expand.addEventListener("click", () => this.expandArticle());
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    console.log(this.domElement);
+    this.domElement.classList.toggle("article-open")
+    
+    
 
   }
 }
